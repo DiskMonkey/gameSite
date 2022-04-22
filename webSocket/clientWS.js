6be1c1296835a0050ee82609c95d5398374c71a2
Mukul Ramesh <mukulplace@gmail.com>
2022-04-22 12:44:56 -0400

    IP

--- webSocket/clientWS.js
+++ webSocket/clientWS.js
@@ -2,8 +2,8 @@ function initWebSocket()
 {
 	return new Promise(function (resolve, reject)
 	{
-		var ws = new WebSocket('ws://52.207.149.38:5050'); //this is ip specific. REMEMBER TO UPDATE WITH EACH NEW INSTANCE. Public IPv4 address
-		//old was 'ws://35.173.136.120:5050'
+		var ws = new WebSocket('ws://172.31.26.18:5050'); //this is ip specific. REMEMBER TO UPDATE WITH EACH NEW INSTANCE. Public IPv4 address
+		//old was 'ws://35.173.136.120:5050'. Update with every tmux session
 		ws.onopen = () =>
 		{
 			console.log('Connection opened!');
