4f2c770f71cafcf8a5cc3eeee446849cfc36b2dc
Mukul Ramesh <mukulplace@gmail.com>
2022-04-21 22:59:43 -0400

    Added footer

--- webSocket/clientWS.js
+++ webSocket/clientWS.js
@@ -2,7 +2,7 @@ function initWebSocket()
 {
 	return new Promise(function (resolve, reject)
 	{
-		var ws = new WebSocket('ws://localhost:5050'); //this is ip specific. REMEMBER TO UPDATE WITH EACH NEW INSTANCE. Public IPv4 address
+		var ws = new WebSocket('ws://52.207.149.38:5050'); //this is ip specific. REMEMBER TO UPDATE WITH EACH NEW INSTANCE. Public IPv4 address
 		//old was 'ws://35.173.136.120:5050'
 		ws.onopen = () =>
 		{
